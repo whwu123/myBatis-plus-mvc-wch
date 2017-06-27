9868546a74c2d8051be4affc425a692d777cf973
= <=>
2017-06-28 00:55:50 +0800

    升级 2.1-beta 新增 实体父类  mapper 父类 测试

--- src/main/java/com/baomidou/springmvc/mapper/system/UserMapper.java
+++ src/main/java/com/baomidou/springmvc/mapper/system/UserMapper.java
@@ -1,6 +1,6 @@
 package com.baomidou.springmvc.mapper.system;
 
-import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.springmvc.common.SuperMapper;
 import com.baomidou.springmvc.model.system.User;
 
 /**
@@ -8,7 +8,7 @@ import com.baomidou.springmvc.model.system.User;
  * User 表数据库控制层接口
  *
  */
-public interface UserMapper extends BaseMapper<User> {
+public interface UserMapper extends SuperMapper<User> {
 
 
 }
--- src/main/java/com/baomidou/springmvc/model/system/User.java
+++ src/main/java/com/baomidou/springmvc/model/system/User.java
@@ -1,64 +1,55 @@
 package com.baomidou.springmvc.model.system;
 
-import java.io.Serializable;
 import java.util.Date;
 
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.FieldIgnore;
+import com.baomidou.springmvc.common.SuperEntity;
 
 /**
- *
  * 系统用户表
- *
  */
 @TableName("sys_user")
-public class User implements Serializable {
+public class User extends SuperEntity {
 
-	private static final long serialVersionUID = 1L;
+    /**
+     * 用户名
+     */
+    private String name;
 
-	/** 用户ID */
-	private Long id;
+    /**
+     * 用户年龄
+     */
+    private Integer age;
 
-	/** 用户名 */
-	private String name;
+    /**
+     * 自定义填充的创建时间
+     */
+    @TableField(ignore = FieldIgnore.INSERT)// 该注解插入忽略验证，自动填充
+    private Date ctime;
 
-	/** 用户年龄 */
-	private Integer age;
+    public String getName() {
+        return this.name;
+    }
 
-	/** 自定义填充的创建时间 */
-	@TableField(ignore = FieldIgnore.INSERT)// 该注解插入忽略验证，自动填充
-	private Date ctime;
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Long getId() {
-		return this.id;
-	}
+    public Integer getAge() {
+        return this.age;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public Date getCtime() {
+        return ctime;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getAge() {
-		return this.age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public Date getCtime() {
-		return ctime;
-	}
-
-	public void setCtime(Date ctime) {
-		this.ctime = ctime;
-	}
+    public void setCtime(Date ctime) {
+        this.ctime = ctime;
+    }
 }
